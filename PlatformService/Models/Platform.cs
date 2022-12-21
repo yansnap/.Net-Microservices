068dcfefa949934d477409daf6674877593ef0c5
ヤン <jan.kozhyn@gmail.com>
2022-12-21 09:03:19 +0200

    add ,gitignore and delete weather controller, add platform class

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Cost { get; set; }
+    }
+}
